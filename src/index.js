69b279b0afac6c6fdaf38346dcbf6ce37c4f4d1f
Maxim Zorin <maxim.zorin@dev.zodiac.tv>
2022-01-09 16:05:33 +0300

    fix: add support Node 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+const flat = arr => arr.reduce((a, row) => a.concat(row), []);
 
 module.exports = function towelSort (matrix) {
-    return matrix ? matrix.map((line, i) => i % 2? line.reverse() : line.slice()).flat() : [];
+    return matrix ? flat(matrix.map((line, i) => i % 2? line.reverse() : line.slice())) : [];
 }
 
 // module.exports = matrix => matrix ? matrix.map((row, i) => row[['slice', 'reverse'][i % 2]]()).flat() : [];
