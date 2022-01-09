543232860dd6ef7359dfd855eeb1458e70cbdc2f
Maxim Zorin <maxim.zorin@dev.zodiac.tv>
2022-01-09 16:01:15 +0300

    fix: add 1line var

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 module.exports = function towelSort (matrix) {
     return matrix ? matrix.map((line, i) => i % 2? line.reverse() : line.slice()).flat() : [];
 }
+
+// module.exports = matrix => matrix ? matrix.map((row, i) => row[['slice', 'reverse'][i % 2]]()).flat() : [];
