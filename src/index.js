95f9026c311eda6930332f3ba1fa7cfb3daaab55
Maxim Zorin <maxim.zorin@zodiacsystems.com>
2022-01-14 02:55:52 +0300

    fix: add immutable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-const flat = arr => arr.reduce((a, row) => a.concat(row), []);
 
 module.exports = function towelSort (matrix) {
-    return matrix ? flat(matrix.map((line, i) => i % 2? line.reverse() : line.slice())) : [];
+    return matrix ? matrix.map(line => line.slice()).map((line, i) => i % 2 ? line.reverse() : line).flat() : [];
 }
-
-// module.exports = matrix => matrix ? matrix.map((row, i) => row[['slice', 'reverse'][i % 2]]()).flat() : [];
